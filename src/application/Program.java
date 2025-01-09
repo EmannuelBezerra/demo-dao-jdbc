ed169753388552f4515698acc6cd6031952dfa00
EmannuelBezerra <emannuelbezerra678@gmail.com>
2025-01-09 17:58:42 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,14 @@
 package application;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1,"Books");
-		DateTimeFormatter form = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		Seller seller  = new Seller(21,"Bob","bob@gmail.com",LocalDate.parse("14/02/2005",form),3000.0,obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
 	}
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entities.Department;
+
+public interface DepartmentDao {
+	 void intert(Department obj);
+	 void update(Department obj);
+	 void deletById(Integer id);
+	 Department findById(Integer id);
+	 List<Department> findAll();
 }
