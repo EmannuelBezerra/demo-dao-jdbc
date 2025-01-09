ea5f9ffe85c61523039b9ab4766137330c15e8bb
EmannuelBezerra <emannuelbezerra678@gmail.com>
2025-01-09 15:03:17 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Department obj = new Department(1,"Books");
+		DateTimeFormatter form = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		Seller seller  = new Seller(21,"Bob","bob@gmail.com",LocalDate.parse("14/02/2005",form),3000.0,obj);
+		System.out.println(seller);
 
 	}
 
